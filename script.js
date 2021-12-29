a7c3733964d56d9997eb850b146ec0bfe653c19c
Bernd Ullmann <berndullmann@Bernds-MacBook-Air.fritz.box>
2021-12-29 20:29:22 +0100

    event listeners

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 'use strict';
 
+// Listener is on the click Button
+document.querySelector('.check').addEventListener('click', function() {
+    //console.log(document.querySelector('.guess').value);
+    const guess = document.querySelector('.guess').value;
+    console.log(typeof guess);
+    //document.querySelector('.message').textContent = 'Correct number';
 
-console.log(document.querySelector('.message').textContentpwd);
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No number';
+    } /*else {
+        document.querySelector('.message').textContent = 'Correct number';
+    }*/
+});
 
-console.log('did it work');
